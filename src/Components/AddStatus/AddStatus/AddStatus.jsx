0b55538c0c0ac75fbbccbcc91a90fdc53a9787a3
Jyotiram-k <96097345+Jyotiram-k@users.noreply.github.com>
2021-12-29 13:06:18 +0530

    add status form

---
+++ src/Components/AddStatus/AddStatus/AddStatus.jsx
@@ -0,0 +1,34 @@
+﻿import React, { Component } from 'react';
+
+
+export default class FormData extends Component {
+    render() {
+        return (
+            <div className="container">
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label>Name</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label>Date</label>
+                        <input type="date" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label>Accomplishments</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label>Impediments</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label>Plan for next day</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-block">Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
